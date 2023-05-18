e50767bcbdfd24054002725274026d14bc34e1b1
CbkAmara <barakamara3@gmail.com>
2023-05-18 15:53:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include stdio.sh
 
+
+/**
+ * main - prints ""Programming is like building a multilingual puzzle
+ *  followed by a new line
+ *  return 0
+ **/ 
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
