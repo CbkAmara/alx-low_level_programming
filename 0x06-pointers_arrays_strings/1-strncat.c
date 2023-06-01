12bfb8beb62b67e96528a1fb0f513efe4b4a60c1
CbkAmara <barakamara3@gmail.com>
2023-06-01 13:25:30 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strncat.c -o 1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: The main string
+ * @src: The string to be appended to main string
+ * @n: The number of bytes from src
+ * Return: A pointer to the resulting string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
+	return (dest);
+}
