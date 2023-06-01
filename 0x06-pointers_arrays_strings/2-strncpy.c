7a1b561224b152a1687f81fedbce33a1304e7263
CbkAmara <barakamara3@gmail.com>
2023-06-01 13:40:51 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies strings
+ * @dest: The coppied string
+ * @src: The source string
+ * @n: no of bytes coppied
+ * Return: a pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	{
+		src_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
+}
+
