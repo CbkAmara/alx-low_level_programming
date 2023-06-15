67d27165b39e0df05415c68de89fe7d3a8e7f678
CbkAmara <barakamara3@gmail.com>
2023-06-15 16:26:34 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - Concatenates two strings with the first n bytes of s2
@@ -14,58 +12,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len, s2_len, concat_len;
-	char *concat;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *s;
 
-	/* Treat NULL as empty strings */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-
-	/* Use the entire s2 string if n is greater or equal to its length */
-	if (n >= s2_len)
-		n = s2_len;
-
-	concat_len = s1_len + n;
-
-	concat = malloc((concat_len + 1) * sizeof(char));
-	if (concat == NULL)
-		return (NULL);
-
-	strncpy(concat, s1, s1_len);
-	strncat(concat, s2, n);
-	concat[concat_len] = '\0';
-
-	return (concat);
-}
-
-/**
- * main - Test the string_nconcat function
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char *s1 = "Best ";
-	char *s2 = "School !!!";
-	unsigned int n = 6;
-
-	char *result = string_nconcat(s1, s2, n);
-
-	if (result != NULL)
-	{
-		printf("Concatenated string: %s\n", result);
-		free(result);
-	}
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 
 	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-		printf("Failed to allocate memory.\n");
+		s[i] = s1[i];
+		i++;
 	}
 
-	return (0);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+
+	return (s);
 }
